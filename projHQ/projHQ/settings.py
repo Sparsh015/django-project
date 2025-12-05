686863dd9bc37f9683c545c4d5eadb10506a7311
sparsh15 <sparsh.1518@gmail.com>
2025-12-06 00:07:09 +0530

    configurations

--- projHQ/projHQ/settings.py
+++ projHQ/projHQ/settings.py
@@ -115,3 +115,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+# Media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
