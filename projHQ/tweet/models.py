e454cbd18eea3327906175ba4852ca01e2e51f7d
sparsh15 <sparsh.1518@gmail.com>
2025-12-11 02:51:00 +0530

    Tweet model, and forms

--- projHQ/tweet/models.py
+++ projHQ/tweet/models.py
@@ -9,4 +9,7 @@ class Tweet(models.Model):
     photo = models.ImageField(upload_to='photos/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user.username} - {self.text[:15]}'
     
