7c4e26c28cd22f56c483f92e34422d0bda8ed9d6
sparsh15 <sparsh.1518@gmail.com>
2025-12-11 03:20:45 +0530

    tweet create view

--- projHQ/tweet/views.py
+++ projHQ/tweet/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Tweet
+from .forms import TweetForm
 
 # Create your views here.
 
 def index(request):
     return render(request, 'index.html')
+
+def tweet_list(request):
+    tweets = Tweet.objects.all().order_by('-created_at')
+    return render(request, 'tweet_list.html', {'tweets': tweets})
+
+def tweet_create(request):
+    if request.method == "POST":
+        form = TweetForm(request.POST, request.FILES)
+        if(form.is_valid()):
+            tweet = form.save(commit=False)
+            tweet.user = request.user
+            tweet.save()
+            return redirect('tweet_list')
+    else:
+        form = TweetForm()
+    
+    return render(request, 'tweet_create.html', {'form': form})
+
