165995e89726e97fbff4202cea2632c7a916d32a
sparsh15 <sparsh.1518@gmail.com>
2025-12-15 00:39:44 +0530

    usercreation form and registration form

--- projHQ/tweet/forms.py
+++ projHQ/tweet/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Tweet
-from django.contrib.auth.forms import UserCreationForm, User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class TweetForm(forms.ModelForm):
     class Meta:
@@ -8,9 +9,8 @@ class TweetForm(forms.ModelForm):
         fields = ['text', 'photo']
         
 
-class UserRegisterationForm():
+class UserRegistrationForm(UserCreationForm):
     email = forms.EmailField(required=True)
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
-        
--- projHQ/tweet/views.py
+++ projHQ/tweet/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from .models import Tweet
-from .forms import TweetForm, UserRegisterationForm
+from .forms import TweetForm, UserRegistrationForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login
 
@@ -57,7 +57,7 @@ def tweet_delete(request, tweet_id):
 
 def register(request):
     if request.method == "POST":
-        form = UserRegisterationForm(request.POST)
+        form = UserRegistrationForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
             user.set_password(form.cleaned_data['password1'])
@@ -65,6 +65,6 @@ def register(request):
             login(request, user)
             return redirect('tweet_list')
     else:
-        form = UserRegisterationForm()
+        form = UserRegistrationForm()
 
-    return render(request, 'registeration.html', {'form' : form})
+    return render(request, 'registration/register.html', {'form' : form})
